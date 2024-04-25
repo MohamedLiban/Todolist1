eb7228cbdd4721f10bbe8cae2db580fd8d8cf074
MohamedLiban <101471661+MohamedLiban@users.noreply.github.com>
2024-04-25 16:57:34 +0200

    Buble sort imp

--- Todolist1/ToDolistmanger.cs
+++ Todolist1/ToDolistmanger.cs
@@ -36,10 +36,19 @@ namespace ToDoListApp
 
         public void SortTasksByPriority()
         {
-            tasks.Sort((x, y) => x.Priority.CompareTo(y.Priority));
+            int n = tasks.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (tasks[j].Priority > tasks[j + 1].Priority)
+                    {
+                        Task temp = tasks[j];
+                        tasks[j] = tasks[j + 1];
+                        tasks[j + 1] = temp;
+                    }
+                }
+            }
         }
-
-
-
     }
 }
